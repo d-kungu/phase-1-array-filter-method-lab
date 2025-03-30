452d744c167e9246f73dcca86be6b7e707bd4e21
Andrew Kungu <drewskungu@gmail.com>
2025-03-31 00:24:16 +0300

    find

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+drivers = [
+    { 
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+function findMatching(drivers, name ){
+        return drivers.filter(driver=> driver.toLowerCase() === name.toLowerCase())
+}
+ 
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver=> driver.toLowerCase().startsWith(name.toLowerCase()))
+}
+
+function matchName(drivers, place){
+    return  drivers.filter(driver=> driver.name === place);
+}
